aa1dcf6a613cef98d3d5351dcbfc5004ef378d29
BOMEN <bomen21x@gmail.com>
2022-11-21 00:55:08 +0700

    Update __manifest__.py

--- ctp_htmx/__manifest__.py
+++ ctp_htmx/__manifest__.py
@@ -15,7 +15,7 @@
     "description": """ 
             Install htmx in Odoo Frontend
             .""",
-    "author": "Cybernetics+",
+    "author": "Cybernetics Plus",
     "website": "https://www.cybernetics.plus",
     "live_test_url": "https://www.cybernetics.plus",
     "images": ["static/description/banner.gif"],
